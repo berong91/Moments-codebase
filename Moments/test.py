000b581af4893da36779e0ea4683afa41ca65be0
Dimitry Rakhlei <dimitryrakhlei@gmail.com>
2017-06-25 14:08:03 -0700

    working test.py

--- Moments/test.py
+++ Moments/test.py
@@ -9,6 +9,9 @@ from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import optimizers
 from keras.models import load_model
 
+#(beween 0 and 6: anger=0, disgust=1, fear=2, happy=3, sad=4, surprise=5, neutral=6)
+labels = {0 : "anger" , 1 : "disgust" ,  2 : "fear", 3 : "happy", 4 : "sad" , 5 : "surprise" ,  6 : "neutral"}
+
 data = pd.read_csv("train.csv")
 
 im = data['Pixels']
@@ -28,3 +31,4 @@ pred_x = np.reshape(pred_x,(1,48,48,1))
 model = load_model("model.h5")
 p = model.predict(pred_x)
 print(p)
+print(labels[np.argmax(p)])
