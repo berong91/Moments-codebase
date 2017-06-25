358c6d68d7532ddb042ea3e9e02634b0d3b55f36
Dimitry Rakhlei <dimitryrakhlei@gmail.com>
2017-06-24 17:49:35 -0700

    folder structure added

--- Moments/Moments.py
+++ Moments/Moments.py
@@ -1,1 +1,21 @@
+import numpy as np
+import kaggle as kg
+
+
+class Moments:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def ClassifyImage(image):
+        pass
+
+    def GetFaces(image):
+        pass
+
+    def ClassifyFace(image):
+        pass
+    
+    def StoreImage(image, directory):
+        pass
+
 
