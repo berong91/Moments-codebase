33c2ac3be1b6b4e163c4ea1f08b245f42c8b4404
Dimitry Rakhlei <dimitryrakhlei@gmail.com>
2017-06-24 20:24:15 -0700

    dataset added

--- Moments/Moments.py
+++ Moments/Moments.py
@@ -20,10 +20,19 @@ class Moments :
 import pandas as pd
 from PIL import Image
 import numpy as np
-
-for file in pd.read_csv("train.csv", sep=",", chunksize = 1) :
-    data = np.array([int(x) for x in file.get("Pixels")[0].split()])    
-    data = np.reshape(data, (48,48))
-    img = Image.fromarray(data)
-    img.show()
-    break
+import math
+from matplotlib import pyplot
+import scipy.misc as misc
+num = 0
+for file in pd.read_csv("train.csv",  chunksize = 1) :
+    try :
+        data = np.array([int(x) for x in file["Pixels"].iloc[0].split()])
+        data = np.resize(data, (48,48))
+        misc.imsave("faces/image{0}_{1}.png".format(num, file["Emotion"].iloc[0]), data)
+        print(num)
+        #img = Image.fromarray(data, "RGB")
+        #img.save("faces/image{0}.png".format(num))
+        num+=1
+    except Exception as e:
+        print(e)
+        break
